01bb429e61c2ae9d561a1296f00d366f9ababf96
ajlopez <webmaster@ajlopez.com>
2017-11-18 18:30:22 -0300

    Solve two words

--- test/solve.js
+++ test/solve.js
@@ -11,4 +11,26 @@ exports['solve one word'] = function (test) {
 	test.equal(result[0], 'maps');
 }
 
+exports['solve two words returning one word'] = function (test) {
+	var tree = mh.tree().add(['maps', 'foo']);
+	
+	var result = tree.solve(['p', 'a', 'm', 's']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'maps');
+}
+
+exports['solve two words returning two words'] = function (test) {
+	var tree = mh.tree().add(['maps', 'spam']);
+	
+	var result = tree.solve(['p', 'a', 'm', 's']);
+	
+	test.ok(result);
+	test.equal(result.length, 2);
+	test.equal(result[0], 'maps');
+	test.equal(result[1], 'spam');
+}
+
+
 
