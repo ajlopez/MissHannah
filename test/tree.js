0e476408c9881637634b91a9925de764b39d3bf0
ajlopez <webmaster@ajlopez.com>
2017-11-18 18:10:27 -0300

    Add two words

--- test/tree.js
+++ test/tree.js
@@ -55,3 +55,19 @@ exports['add word twice'] = function (test) {
 	test.ok(words.f.o.o.$);
 }
 
+exports['add two words'] = function (test) {
+	var tree = mh.tree();
+	tree.add('foo')
+		.add('four');
+	
+	var words = tree.words();
+	
+	test.ok(words);
+	test.ok(words.f);
+	test.ok(words.f.o);
+	test.ok(words.f.o.o);
+	test.ok(words.f.o.o.$);
+	test.ok(words.f.o.u);
+	test.ok(words.f.o.u.r);
+	test.ok(words.f.o.u.r.$);
+}
