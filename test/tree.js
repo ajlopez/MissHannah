f7c08fb58354b1e9614d5e78123660e286553052
ajlopez <webmaster@ajlopez.com>
2017-11-18 17:45:06 -0300

    No words in empty tree

--- test/tree.js
+++ test/tree.js
@@ -8,3 +8,12 @@ exports['create tree as object'] = function (test) {
 	test.equal(typeof tree, 'object');
 }
 
+exports['no words'] = function (test) {
+	var tree = mh.tree();
+	var words = tree.words();
+	
+	test.ok(words);
+	test.equal(typeof words, 'object');
+	test.equal(Object.keys(words).length, 0);
+}
+
