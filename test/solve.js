c42fe98b67e51d8481d613d070e334c7d9b4ee12
ajlopez <webmaster@ajlopez.com>
2017-11-19 12:14:25 -0300

    Use repeated vowels

--- test/solve.js
+++ test/solve.js
@@ -30,6 +30,56 @@ exports['solve two words returning one word'] = function (test) {
 	test.equal(result[0], 'maps');
 }
 
+exports['solve word repeating vowel a'] = function (test) {
+	var tree = mh.tree().add(['maps', 'papa']);
+	
+	var result = tree.solve(['a', 'p', 'p']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'papa');
+}
+
+exports['solve word repeating vowel e'] = function (test) {
+	var tree = mh.tree().add(['maps', 'fee']);
+	
+	var result = tree.solve(['e', 'f']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'fee');
+}
+
+exports['solve word repeating vowel i'] = function (test) {
+	var tree = mh.tree().add(['maps', 'bikini']);
+	
+	var result = tree.solve(['i', 'b', 'k', 'n']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'bikini');
+}
+
+exports['solve word repeating vowel o'] = function (test) {
+	var tree = mh.tree().add(['maps', 'foo']);
+	
+	var result = tree.solve(['o', 'f']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'foo');
+}
+
+exports['solve word repeating vowel u'] = function (test) {
+	var tree = mh.tree().add(['maps', 'bululu']);
+	
+	var result = tree.solve(['u', 'b', 'l', 'l']);
+	
+	test.ok(result);
+	test.equal(result.length, 1);
+	test.equal(result[0], 'bululu');
+}
+
 exports['solve two words no result'] = function (test) {
 	var tree = mh.tree().add(['maps', 'foo']);
 	
